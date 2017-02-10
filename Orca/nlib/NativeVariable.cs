7c8c7566cb7fdece9e47c1ce3fe9043eed3f96ea
inbread <solsam10@naver.com>
2017-02-10 22:30:26 +0900

    nlib done
    still need to change get range

---
+++ Orca/nlib/NativeVariable.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Orca.nlib
+{
+    /**
+     * 네이티브 변수
+     * 
+     * @author 김 현준
+     */
+    class NativeVariable
+    {
+
+        public string variableName;
+        public string value;
+
+        public NativeVariable(string variableName, string value)
+        {
+            this.value = value;
+            this.variableName = variableName;
+        }
+
+    }
+}
